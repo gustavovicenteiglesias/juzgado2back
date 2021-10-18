0c771cebbcdd772769cfa262cc5f3d43c631ee98
Gustavo Iglesias <gustavo.iglesias40@gmail.com>
2021-10-18 11:45:32 -0300

    cedulaTitular

--- src/main/java/com/bezkoder/spring/data/jpa/pagingsorting/controller/PdfController.java
+++ src/main/java/com/bezkoder/spring/data/jpa/pagingsorting/controller/PdfController.java
@@ -94,10 +94,40 @@ public class PdfController {
 		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, para,new JREmptyDataSource());
 		
 		response.setContentType("application/pdf");
-		response.addHeader("Content-Disposition", "inline; filename=abono"+id+".pdf;");
+		response.addHeader("Content-Disposition", "inline; filename=cedula"+id+".pdf;");
 		
 		final OutputStream outStream = response.getOutputStream();
 	    JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
 	}
+
+
+@GetMapping(value = "/api/cedulatitular/{id}")
+
+public void getDocumentcedulaTitular(HttpServletResponse response,@PathVariable("id") Long id) throws IOException, JRException, SQLException {
+
+	Infraccione infraccion= infracionesRepository.findById(id).get();
+	InputStream jasperStream = this.getClass().getResourceAsStream("/reports/Cedula.jrxml");
+	Map <String,Object> para = new HashMap<>();
+	para.put("causa", infraccion.getCausa());
+	para.put("nombre", infraccion.getNombreTitular());
+	para.put("acta", infraccion.getActa());
+	para.put("ley_ordenanza", infraccion.getLeyOrdenanza());
+	para.put("articulo", infraccion.getArticulo());
+	para.put("dominio",infraccion.getDominio());
+	para.put("fecha", infraccion.getFecha());
+	para.put("direccion", infraccion.getDireccionTitular());
+	para.put("localidad", infraccion.getLocalidadTitular());
+	para.put("provincia", infraccion.getProvinciaTitular());
+	para.put("codigo_postal", infraccion.getCpTitular());
+	
+	JasperReport jasperReport = JasperCompileManager.compileReport(jasperStream);
+	JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, para,new JREmptyDataSource());
+	
+	response.setContentType("application/pdf");
+	response.addHeader("Content-Disposition", "inline; filename=cedulatitular"+id+".pdf;");
+	
+	final OutputStream outStream = response.getOutputStream();
+    JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+}
 	
 }
