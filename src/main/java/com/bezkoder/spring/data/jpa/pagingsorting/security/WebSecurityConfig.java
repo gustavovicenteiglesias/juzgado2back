d2d0506a87e77aab87d82fc78bab9ac8933936f2
Gustavo Iglesias <gustavo.iglesias40@gmail.com>
2026-01-09 14:21:13 -0300

    cors  "https://test.juzgado2.areco.ar",     "https://www.test.juzgado2.areco.ar"

--- src/main/java/com/bezkoder/spring/data/jpa/pagingsorting/security/WebSecurityConfig.java
+++ src/main/java/com/bezkoder/spring/data/jpa/pagingsorting/security/WebSecurityConfig.java
@@ -67,8 +67,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 "http://localhost:3000",
                 "https://juzgado2.areco.gob.ar",
                 "https://apijuzgado2.areco.ar",
-                "https://test.juzgado2.areco.gob.ar",
-                "https://www.test.juzgado2.areco.gob.ar"
+                "https://test.juzgado2.areco.ar",
+                "https://www.test.juzgado2.areco.ar"
 
         ));
         configuration.setAllowedMethods(Arrays.asList(
